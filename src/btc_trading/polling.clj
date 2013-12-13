71c672b92eff41deb441de442a1ce842752cda42
gilmaso <gilmasog@gmail.com>
2013-12-12 22:07:52 -0600

    toyed around some more

--- src/btc_trading/polling.clj
+++ src/btc_trading/polling.clj
@@ -1,16 +1,17 @@
 (ns btc-trading.polling
   (:require [btc-trading.btc-china :as china]
-            [clojure.core.async :as async :refer [chan close! go
-                                                  >! >!! <! <!!
-                                                  thread timeout]]))
+            [clojure.core.async :as async :refer [>!! <!!
+                                                  chan close! go
+                                                  sliding-buffer]]))
 
 
-(let [c (chan)]
+(let [c (chan (sliding-buffer 10))]
   (future
     (>!! c (china/get-account-info))
     (>!! c (china/get-market-depth 10)))
   (println (<!! c))
-  (println (<!! c)))
+  (println (<!! c))
+  (close! c))
 
 
 

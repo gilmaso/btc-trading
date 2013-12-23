f7600b58cd026c86a3b3fceade57721b449984ff
gilmaso <gilmasog@gmail.com>
2013-12-23 16:56:32 -0600

    created spawn-thread

--- src/btc_trading/polling.clj
+++ src/btc_trading/polling.clj
@@ -1,47 +1,19 @@
-(ns btc-trading.polling
-  (:require [btc-trading.btc-china :as china]
-            [clojure.core.async :as async :refer [>!! <!!
-                                                  chan close! go
-                                                  sliding-buffer thread]]))
+(ns btc-trading.polling)
 
 
+(defn spawn-thread [function interval]
+  "Takes a function to be repeatedly called at a given interval.
+  Returns a kill switch
+  Takes: function
+        interval in milliseconds
+  Returns: promise"
+  (let [kill-switch (promise)]
+    (future (while (not (realized? kill-switch))
+              (do
+                (Thread/sleep interval)
+                (eval function))))
+    kill-switch))
 
-(defn poll [function sleep]
-  (let [c (chan (sliding-buffer 10))
-        kill-switch (promise)]
-    (while (not (realized? kill-switch))
-      (do
-        (Thread/sleep sleep)
-        (>!! c (function))))
-    {:channel c :kill-switch kill-switch}))
-
-
-(defn infinite-loop [function]
-  (do
-    (function)
-    (future (infinite-loop function))
-    nil)) ;; nil is necessary to avoid overflowing the stack with futures...
-
-
-(def counter (atom 1))
-
-(infinite-loop
- #(do
-    (Thread/sleep 1000)
-    (swap! counter inc)))
-
-@counter
-
-
-(def channel (chan (sliding-buffer 10)))
-(infinite-loop
- #(do
-    (Thread/sleep 5000)
-    (>!! channel (china/get-market-depth 3))))
-
-(println (<!! channel))
-
-; I should consider using netty
 
 
 

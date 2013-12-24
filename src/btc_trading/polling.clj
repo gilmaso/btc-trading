3fee1df19733f4581dfb74758a776d811f7afd6c
gilmaso <gilmasog@gmail.com>
2013-12-24 12:27:30 -0600

    moved jobs logic into polling

--- src/btc_trading/polling.clj
+++ src/btc_trading/polling.clj
@@ -1,18 +1,39 @@
 (ns btc-trading.polling)
 
 
-(defn spawn-thread [function interval]
+(def ^:private spawned-threads (atom {}))
+
+(defn spawn-thread [function interval name]
   "Takes a function to be repeatedly called at a given interval.
-  Returns a kill switch
+  Returns a uuid.
+  Use the uuid to acc.
   Takes: function
-        interval in milliseconds
+         interval in milliseconds
   Returns: promise"
-  (let [kill-switch (promise)]
-    (future (while (not (realized? kill-switch))
+  (let [kill-switch (promise)
+        uuid (keyword (str (java.util.UUID/randomUUID)))
+        start-time (System/currentTimeMillis)]
+    (future
+      (while (not (realized? kill-switch))
               (do
                 (Thread/sleep interval)
-                (eval function))))
-    kill-switch))
+                (eval function)))
+      (swap! spawned-threads dissoc uuid)) ; Remove thread from spawned-threads listing
+    (swap! spawned-threads conj {uuid
+                                   {:name name
+                                    :function function
+                                    :start-time start-time
+                                    :kill-switch kill-switch}}) ; Add threads to spawned-threads
+    uuid)) ; Return the uuid to caller
+
+(defn get-spawned-threads []
+  @spawned-threads)
+
+
+
+
+
+
 
 
 

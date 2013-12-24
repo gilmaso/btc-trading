98b9900365440eb3630203f671dbe13988e0a6b6
gilmaso <gilmasog@gmail.com>
2013-12-24 13:43:00 -0600

    updated gpl; added persistence

--- src/btc_trading/persistence.clj
+++ src/btc_trading/persistence.clj
@@ -15,17 +15,8 @@
 ;
 ; Email: gilmasog@gmail.com
 
-(defproject btc-trading "0.1.0-SNAPSHOT"
-  :description "A simple btc-trading application."
-  :url "https://github.com/gilmaso/btc-trading"
-  :license {:name "GNU GPL V3"
-            :url "http://www.gnu.org/licenses/gpl.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [cheshire "5.2.0"]
-                 [http-kit "2.1.13"]
-                 [org.clojure/data.codec "0.1.0"]
-                 [org.clojure/core.async "0.1.262.0-151b23-alpha"]
-                 [com.taoensso/carmine "2.4.0"]]
-  :main ^:skip-aot btc-trading.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+(ns btc-trading.presistence
+  (:require [taoensso.carmine :as car :refer (wcar)]))
+
+(def server1-conn {:pool {<opts>} :spec {<opts>}})
+(defmacro wcar* [& body] `(car/wcar server1-conn ~@body))
